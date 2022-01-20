7c4a1c5606664e9008ca8689c53e85f0bf3dd880
foxhsx <932163475@qq.com>
2022-01-21 00:17:02 +0800

    喝水和电影

---
+++ movie-app/index.js
@@ -0,0 +1,66 @@
+const API_URL = "https://api.wmdb.tv/api/v1/movie/search?q=";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+async function getMovie(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  console.log(res, data, "ddd");
+  showMovies(data);
+}
+
+function showMovies(movies) {
+  main.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const { doubanRating, data } = movie;
+    const movieInfo = data[0];
+    const { poster, name, description, country, genre, language } = movieInfo;
+
+    const moiveEl = document.createElement("div");
+    moiveEl.classList.add("movie");
+
+    moiveEl.innerHTML = `
+      <img src="${poster}" alt="${name}" />
+      <div class="movie-info">
+        <h3>${name}</h3>
+        <span class="${getClassByRate(doubanRating)}">${doubanRating}</span>
+      </div>
+      <div class="overview">
+        <h3>概述</h3>
+        <p><span>发行地区：</span><span>${country}</span></p>
+        <p><span>分类标签：</span><span>${genre}</span></p>
+        <p><span>语言：</span><span>${language}</span></p>
+        <p><span>情节：</span><span>${description}</span></p>
+      </div>
+    `;
+    main.appendChild(moiveEl);
+  });
+}
+
+function getClassByRate(rate) {
+  if (rate >= 8) {
+    return "green";
+  } else if (rate >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm && searchTerm !== "") {
+    getMovie(API_URL + searchTerm);
+
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
