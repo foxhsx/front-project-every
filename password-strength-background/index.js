b87f7e3bc2e30561068bdec432505d3823587fd0
foxhsx <932163475@qq.com>
2022-02-09 00:29:24 +0800

    密码强度背景

---
+++ password-strength-background/index.js
@@ -0,0 +1,26 @@
+const note = document.getElementById("note");
+
+note.addEventListener("click", async (e) => {
+  if (note.classList.length > 0) {
+    note.innerHTML = "Note";
+    note.classList.remove("show");
+    return;
+  }
+  const { data } = await axios("./README.md");
+  note.classList.add("show");
+  const close = document.createElement("div");
+  close.innerHTML = "X";
+  close.classList.add("close");
+  note.innerHTML = marked.parse(data);
+  note.appendChild(close);
+});
+
+const password = document.getElementById("password");
+const background = document.getElementById("background");
+
+password.oninput = function (e) {
+  const input = e.target.value;
+  const length = input.length;
+  const blurValue = 20 - length * 2;
+  background.style.filter = `blur(${blurValue}px)`;
+};
