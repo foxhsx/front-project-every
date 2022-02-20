1f3e82225ba7c23da95990f1060467ca174674d0
foxhsx <932163475@qq.com>
2022-02-20 09:54:36 +0800

    拍虫子

---
+++ catch-the-insect/index.js
@@ -0,0 +1,101 @@
+const note = document.getElementById("note");
+
+note.addEventListener("click", async (e) => {
+  if (note.classList.length > 0) {
+    note.innerHTML = "Note";
+    note.classList.remove("show");
+    return;
+  }
+  const { data } = await axios("./README.md");
+  note.classList.add("show");
+  const close = document.createElement("div");
+  close.innerHTML = "X";
+  close.classList.add("close");
+  note.innerHTML = marked.parse(data);
+  note.appendChild(close);
+});
+
+const screens = document.querySelectorAll(".screen");
+const choose_insect_btns = document.querySelectorAll(".choose-insect-btn");
+const start_btn = document.getElementById("start-btn");
+const game_container = document.getElementById("game-container");
+const timeEl = document.getElementById("time");
+const scoreEl = document.getElementById("score");
+const message = document.getElementById("message");
+
+let seconds = 0;
+let score = 0;
+let selected_insect = {};
+
+start_btn.onclick = () => screens[0].classList.add("up");
+
+choose_insect_btns.forEach((btn) => {
+  btn.onclick = () => {
+    const img = btn.querySelector("img");
+    const src = img.getAttribute("src");
+    const alt = img.getAttribute("alt");
+
+    selected_insect = { src, alt };
+    screens[1].classList.add("up");
+    setTimeout(createInsect, 1000);
+    startGame();
+  };
+});
+
+function startGame() {
+  setInterval(increaseTime, 1000);
+}
+
+function increaseTime() {
+  let m = Math.floor(seconds / 60);
+  let s = seconds % 60;
+  m = m < 10 ? `0${m}` : m;
+  s = s < 10 ? `0${s}` : s;
+  timeEl.innerHTML = `Time: ${m}:${s}`;
+  seconds++;
+}
+
+function createInsect() {
+  const insect = document.createElement("div");
+  insect.classList.add("insect");
+  const { x, y } = getRandomLocation();
+  insect.style.top = `${y}px`;
+  insect.style.left = `${x}px`;
+  insect.innerHTML = `<img src="${selected_insect.src}" alt="${
+    selected_insect.alt
+  }" style="transform: rotate(${Math.random() * 360}deg)"  />`;
+
+  insect.onclick = catchInsect;
+
+  game_container.appendChild(insect);
+}
+
+function getRandomLocation() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  const x = Math.random() * (width - 200) + 100;
+  const y = Math.random() * (height - 200) + 100;
+
+  return { x, y };
+}
+
+function catchInsect() {
+  increaseScore();
+  this.classList.add("caught");
+  setTimeout(() => this.remove(), 2000);
+  addInsects();
+}
+
+function addInsects() {
+  setTimeout(createInsect, 1000);
+  setTimeout(createInsect, 1500);
+}
+
+function increaseScore() {
+  score++;
+  if (score > 19) {
+    message.classList.add("visible");
+  }
+  scoreEl.innerHTML = `Score: ${score}`;
+}
